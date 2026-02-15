5978823d8052cd6ab7c35bfe2521e465fe0fe5bb
amail912 <alexadressejeux@laposte.net>
2026-02-15 11:06:25 +0100

    Add spec test suite and run it alongside legacy tests

--- test/Main.purs
+++ test/Main.purs
@@ -3,10 +3,13 @@ module Test.Main where
 import Prelude
 
 import Effect (Effect)
+import Effect.Aff (launchAff_)
 import Effect.Class.Console (log)
 import Test.LegacyTests (runLegacyTests)
+import Test.SpecSuite (runSpecSuite)
 
 main :: Effect Unit
 main = do
   runLegacyTests
   log "✅ All legacy tests passed"
+  launchAff_ runSpecSuite
