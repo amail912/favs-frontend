902fafc4c9aae21930fd688c45226bd607d1ddcf
amail912 <alexadressejeux@laposte.net>
2026-02-15 11:06:29 +0100

    Migrate fully from legacy tests to SpecSuite

--- test/Main.purs
+++ test/Main.purs
@@ -4,12 +4,7 @@ import Prelude
 
 import Effect (Effect)
 import Effect.Aff (launchAff_)
-import Effect.Class.Console (log)
-import Test.LegacyTests (runLegacyTests)
 import Test.SpecSuite (runSpecSuite)
 
 main :: Effect Unit
-main = do
-  runLegacyTests
-  log "✅ All legacy tests passed"
-  launchAff_ runSpecSuite
+main = launchAff_ runSpecSuite
